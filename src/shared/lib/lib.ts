c1474be642a41863cbbad2a0b9b5ef55305a336b
Ivan <ivan-kostyulin@yandex.ru>
2026-06-01 11:43:40 +0300

    fix: order links

--- src/shared/lib/lib.ts
+++ src/shared/lib/lib.ts
@@ -5,5 +5,5 @@ export const SOCIAL_MAX =
 	'https://max.ru/join/1FxWSQiUGf11mLzQK5181cVwiXrVAdHMi5oNLqnCypQ';
 export const SOCIAL_VK = 'https://vk.com/contestmiit';
 
-export const REGULATION_LINK = 'https://cloud.mail.ru/public/gH9D/1i6v9BfLL';
-export const ORDER_LINK = 'https://cloud.mail.ru/public/6QGF/uVM5uKeXM';
+export const REGULATION_LINK = 'https://cloud.mail.ru/public/Y4os/ap8ue7EfA';
+export const ORDER_LINK = 'https://cloud.mail.ru/public/4bsq/GoeQ1d8cx';
